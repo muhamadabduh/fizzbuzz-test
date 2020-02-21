937dc38f3cff3a35f5fc566479aa7c6f18b98b2a
Muhamad Abduh <duh.abduh@gmail.com>
2020-02-21 09:26:02 +0700

    ready to go

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -9,20 +9,4 @@ describe('Fizzbuzz test', function() {
     
   });
 
-  it('should return "fizz" if given number can be divided by 3', function() {
-    let output = fizzbuzz(6);
-    expect(output).to.be.a("string");
-    expect(output).to.be.equal("fizz");
-  });
-
-  it('should return "buzz" if given number can be divided by 5', function() {
-    expect(fizzbuzz(5)).to.be.a("string");
-    expect(fizzbuzz(5).to.be.equal("buzz"));
-  });
-
-  it('should return "fizzbuzz" if given number can be divided by 15', function() {
-    let output = fizzbuzz(15);
-    expect(output).to.be.a("string");
-    expect(output).to.be.equal("fizzbuzz");
-  });
 });
